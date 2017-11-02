2e1b26719ca3e38b2170aa62aed0070c3bcd6ef8
badatfun <j.c.delafields@gmail.com>
2017-11-02 14:41:04 -0400

    Done function mapping current btc price to nokia model

--- index.py
+++ index.py
@@ -1,8 +1,21 @@
 import requests
 from nokia import nokia_phones
 
-reqBTCPrice = requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD')
-#BTCPrice = int(round(float(reqBTCPrice.text)), -2)
-test = reqBTCPrice.text[7:14]
+def getBTCPrice():
+    reqBTCPrice = requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD')
+    BTCPrice = int(round(reqBTCPrice.json()['USD'] / 100) * 100)
+    return BTCPrice
 
-print(test)
+def getNokiaPhone():
+    price = getBTCPrice()
+    Found = False
+    Nokiacoin, NokiacoinName, NokiacoinImg = '', '', ''
+
+    for phone in nokia_phones:
+        if(phone == price and Found == False):
+            Found = True
+            Nokiacoin = nokia_phones[phone]
+            NokiacoinName = Nokiacoin.get('name')
+            NokiacoinImg = Nokiacoin.get('image')
+
+    return(Found, NokiacoinName, NokiacoinImg)
