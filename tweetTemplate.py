aa217e274c458203becb3838320ffced5fd8cb11
badatfun <j.c.delafields@gmail.com>
2017-11-05 21:20:10 -0500

    Added tweet template

---
+++ tweetTemplate.py
@@ -0,0 +1,9 @@
+tweets = [
+    'At ${0}, #BTC is a {1}',
+    'POS might be a P.O.S. but at ${0}, #BTC is a {1}',
+    'BTC is worth ${0} right now. Call Satoshi and thank him on your {1}',
+    'Do not sell at ${0}, cell with a {1}',
+    'We have reached ${0}, a {1} will work on the moon',
+    'Now that BTC is at ${0}, call your local representative with your {1} and tell them to abolish the Fed',
+    'With BTC at ${0}...You think JPM traders are buying on their burner {1}?'
+]
--- tweeter.py
+++ tweeter.py
@@ -1,9 +1,11 @@
-from twython import Twython, TwythonError
-from index import *
-from io import BytesIO
 import os
 import dotenv
 import time
+import random
+from twython import Twython, TwythonError
+from io import BytesIO
+from index import *
+from tweetTemplate import tweets
 
 dotenv.load_dotenv('config.env')
 
@@ -12,30 +14,37 @@ consumer_secret = os.environ.get('TWITTER_SECRET_KEY')
 twitter_access = os.environ.get('TWITTER_ACCESS_TOKEN')
 twitter_secret = os.environ.get('TWITTER_ACCESS_SECRET')
 
-twitter = Twython(consumer_key, consumer_secret, twitter_access, twitter_secret)
+twitter = Twython(consumer_key, consumer_secret, twitter_access,
+                  twitter_secret)
+
 
 # If the rounded price matches a phone model,
 # tweets that phone's image
 def fireTweet():
     Found, phoneModel, phoneImage, roundedPrice, price = getNokiaPhone()
-    print("Found: {0} \nModel: {1} \nImg: {2} \nroundedPrice: {3} \nprice: {4}".format(Found, phoneModel, phoneImage, roundedPrice, price))
+    print(
+        "Found: {0} \nModel: {1} \nImg: {2} \nroundedPrice: {3} \nprice: {4}".
+        format(Found, phoneModel, phoneImage, roundedPrice, price))
 
     try:
-        if(Found == True):
+        if (Found == True):
             response = requests.get(phoneImage)
             photo = BytesIO(response.content)
             response = twitter.upload_media(media=photo)
 
-            successTweet = "At ${0}, #BTC is a {1}".format(price, phoneModel)
-            twitter.update_status(status=successTweet, media_ids=[response['media_id']])
+            successTweet = tweets[random.randrange(0, 6)].format(
+                price, phoneModel)
+            twitter.update_status(
+                status=successTweet, media_ids=[response['media_id']])
 
         else:
             #pass
             failTweet = 'NO PHONE AT THIS PRICE (%s) 😫' % price
-            twitter.update_status(status =failTweet)
+            twitter.update_status(status=failTweet)
     except TwythonError as e:
         print(e)
 
     return
+
 
 fireTweet()
